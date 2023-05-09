70f9a0fac177fde91d1a24602eb9b0facbbba95f
NenadoPL1Z <limon4ikpro1@gmail.com>
2023-05-09 20:16:20 +0500

    add: auth

--- src/app.ts
+++ src/app.ts
@@ -3,7 +3,10 @@ import cors from "cors";
 import { PORT } from "@/src/lib/constants/constants";
 import bodyParser from "body-parser";
 import { API_ERROR_USER_AUTH } from "@/src/lib/constants/api/API_ERROR_NAMESPACES";
-import { sendErrorResponse } from "@/src/lib/services/services";
+import {
+  sendErrorResponse,
+  sendSuccessResponse,
+} from "@/src/lib/services/services";
 import { parseUser } from "@/src/lib/services/parser/parseUser";
 
 const app = express();
@@ -17,13 +20,20 @@ app.use(
 app.use(bodyParser.json());
 
 app.post("/auth/login", async (req: express.Request, res: express.Response) => {
+  const sendSuccess = sendSuccessResponse(res);
   const sendError = sendErrorResponse(res);
 
   try {
     const { login, password } = req.body;
 
     if (login && password) {
-      const res = await parseUser(login, password);
+      return await parseUser(login, password)
+        .then((response) => {
+          sendSuccess(response, "userName");
+        })
+        .catch(() => {
+          sendError(400, API_ERROR_USER_AUTH.INVALID_REQUEST);
+        });
     }
     sendError(400, API_ERROR_USER_AUTH.INVALID_DATA);
   } catch (e) {
--- src/lib/services/services.ts
+++ src/lib/services/services.ts
@@ -1,5 +1,12 @@
 import express from "express";
 
+export const sendSuccessResponse = (res: express.Response) => {
+  return (data: any, key = "data"): void => {
+    res.status(200);
+    res.json({ [key]: data });
+  };
+};
+
 export const sendErrorResponse = (res: express.Response) => {
   return (status: number, error: string): void => {
     res.status(status);
