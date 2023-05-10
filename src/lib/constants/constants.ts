706818364f62e1dc71b2a1082a2d324fc5cbfedc
NenadoPL1Z <limon4ikpro1@gmail.com>
2023-05-10 21:12:50 +0500

    update: local-variables

--- src/lib/constants/constants.ts
+++ src/lib/constants/constants.ts
@@ -1,9 +1,7 @@
 import env from "dotenv";
 env.config();
-export const PORT = process.env.PORT;
+export const PORT = 3000;
 export const ADMIN_LOGIN = process.env.ADMIN_LOGIN || "";
 export const ADMIN_PASSWORD = process.env.ADMIN_PASSWORD || "";
 
-export const SCHEDULE_UPDATE_INTERVAL = +(
-  process.env.SCHEDULE_UPDATE_INTERVAL || 120000
-);
+export const SCHEDULE_UPDATE_INTERVAL = 600000;
