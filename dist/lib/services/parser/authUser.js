1f46f8036596cc897b750c03fb8e47b6de47f9da
NenadoPL1Z <limon4ikpro1@gmail.com>
2023-05-11 22:35:39 +0500

    update: deploy

--- dist/lib/services/parser/authUser.js
+++ dist/lib/services/parser/authUser.js
@@ -4,17 +4,40 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.checkAuthUser = exports.authUser = void 0;
-const puppeteer_core_1 = __importDefault(require("puppeteer-core"));
+const chrome_aws_lambda_1 = __importDefault(require("chrome-aws-lambda"));
+const constants_1 = require("../../constants/constants");
 const LOGIN_URL = "https://moodle.preco.ru/login/index.php";
 const LOGIN_INPUT_SELECTOR = "#username";
 const PASSWORD_INPUT_SELECTOR = "#password";
 const SEND_BUTTON_SELECTOR = "#loginbtn";
+let puppeteer = undefined;
+if (constants_1.IS_VERSCEL) {
+    // running on the Vercel platform.
+    puppeteer = require("puppeteer-core");
+}
+else {
+    // running locally.
+    puppeteer = require("puppeteer");
+}
 const authUser = async (login, password) => {
     try {
-        const browser = await puppeteer_core_1.default.launch({
-            headless: false,
-            executablePath: puppeteer_core_1.default.executablePath("chrome"),
-        });
+        const puppeteerConfig = constants_1.IS_VERSCEL
+            ? {
+                args: [
+                    ...chrome_aws_lambda_1.default.args,
+                    "--hide-scrollbars",
+                    "--disable-web-security",
+                ],
+                defaultViewport: chrome_aws_lambda_1.default.defaultViewport,
+                executablePath: await chrome_aws_lambda_1.default.executablePath,
+                ignoreHTTPSErrors: true,
+                headless: true,
+            }
+            : {
+                headless: false,
+                executablePath: puppeteer.executablePath("chrome"),
+            };
+        const browser = await puppeteer.launch(puppeteerConfig);
         const page = await browser.newPage();
         await page.goto(LOGIN_URL);
         await page.setViewport({ width: 1920, height: 1080 });
--- src/lib/services/parser/authUser.ts
+++ src/lib/services/parser/authUser.ts
@@ -1,20 +1,46 @@
-import puppeteer from "puppeteer-core";
+import chromium from "chrome-aws-lambda";
 import { AuthUserFunction } from "../../../types/types";
 import { BrowserModel } from "../../models/BrowserModel";
+import { PuppeteerLaunchOptions, PuppeteerNode } from "puppeteer-core";
+import { IS_VERSCEL } from "../../constants/constants";
 
 const LOGIN_URL = "https://moodle.preco.ru/login/index.php";
 const LOGIN_INPUT_SELECTOR = "#username";
 const PASSWORD_INPUT_SELECTOR = "#password";
 const SEND_BUTTON_SELECTOR = "#loginbtn";
 
+let puppeteer = undefined as unknown as PuppeteerNode;
+
+if (IS_VERSCEL) {
+  // running on the Vercel platform.
+  puppeteer = require("puppeteer-core");
+} else {
+  // running locally.
+  puppeteer = require("puppeteer");
+}
+
 export const authUser: AuthUserFunction<
   Promise<BrowserModel | unknown>
 > = async (login, password) => {
   try {
-    const browser = await puppeteer.launch({
-      headless: false,
-      executablePath: puppeteer.executablePath("chrome"),
-    });
+    const puppeteerConfig: PuppeteerLaunchOptions = IS_VERSCEL
+      ? {
+          args: [
+            ...chromium.args,
+            "--hide-scrollbars",
+            "--disable-web-security",
+          ],
+          defaultViewport: chromium.defaultViewport,
+          executablePath: await chromium.executablePath,
+          ignoreHTTPSErrors: true,
+          headless: true,
+        }
+      : {
+          headless: false,
+          executablePath: puppeteer.executablePath("chrome"),
+        };
+
+    const browser = await puppeteer.launch(puppeteerConfig);
     const page = await browser.newPage();
 
     await page.goto(LOGIN_URL);
