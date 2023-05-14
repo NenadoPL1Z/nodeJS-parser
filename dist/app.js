bbaed59361a6aba79672ba3a3619de5341699183
NenadoPL1Z <limon4ikpro1@gmail.com>
2023-05-14 23:49:07 +0500

    change: deploy

---
+++ dist/app.js
@@ -0,0 +1,51 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.ScheduleModel = exports.sequelize = void 0;
+const express_1 = __importDefault(require("express"));
+const pg_1 = __importDefault(require("pg"));
+const cors_1 = __importDefault(require("cors"));
+const body_parser_1 = __importDefault(require("body-parser"));
+const constants_1 = require("./lib/constants/constants");
+const parseSchedule_1 = require("./lib/services/parser/parseSchedule");
+const sequelize_1 = require("sequelize");
+const getResIndexRoute_1 = require("./lib/services/api/getResIndexRoute");
+const getSchedule_1 = require("./lib/services/api/getSchedule");
+const getUser_1 = require("./lib/services/api/getUser");
+const app = (0, express_1.default)();
+exports.sequelize = new sequelize_1.Sequelize("postgres://admin:omibTSgMhq7VG92uozcDXOsud7UMrg4J@dpg-chgb95u7avjbbju9hui0-a.oregon-postgres.render.com/preco", {
+    dialect: "postgres",
+    dialectModule: pg_1.default,
+    dialectOptions: {
+        ssl: true,
+    },
+});
+exports.ScheduleModel = exports.sequelize.define("Schedule", {
+    id: { type: sequelize_1.DataTypes.INTEGER, primaryKey: true, autoIncrement: false },
+    ruUpdateTime: { type: sequelize_1.DataTypes.STRING },
+    result: { type: sequelize_1.DataTypes.STRING(300000) },
+}, { tableName: "Schedule", freezeTableName: true });
+app.use((0, cors_1.default)({ origin: "*" }));
+app.use(body_parser_1.default.urlencoded({
+    extended: true,
+}));
+app.use(body_parser_1.default.json());
+app.get("/", getResIndexRoute_1.getResIndexRoute);
+app.get("/api/schedule", getSchedule_1.getSchedule);
+app.post("/api/auth/login", getUser_1.getUser);
+app.listen(constants_1.PORT, async () => {
+    console.log(`Example app listening on port ${constants_1.PORT}`);
+    try {
+        await exports.sequelize.authenticate();
+        await exports.sequelize.sync({ force: true });
+        console.log("Connection has been established successfully.");
+    }
+    catch (error) {
+        console.error("Unable to connect to the database:", error);
+    }
+    (0, parseSchedule_1.parseSchedule)().then();
+    setInterval(parseSchedule_1.parseSchedule, constants_1.SCHEDULE_UPDATE_INTERVAL);
+});
+//# sourceMappingURL=app.js.map
---
+++ dist/lib/services/services.js
@@ -0,0 +1,96 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.setScheduleDB = exports.getScheduleData = exports.getGroupListData = exports.sendErrorResponse = exports.sendSuccessResponse = void 0;
+const app_1 = require("../../app");
+const sendSuccessResponse = (res) => {
+    return ({ key = "data", data }) => {
+        res.status(200);
+        res.setHeader("Content-Type", "application/json");
+        res.json({ [key]: data });
+    };
+};
+exports.sendSuccessResponse = sendSuccessResponse;
+const sendErrorResponse = (res) => {
+    return (status, error) => {
+        res.status(status);
+        res.json({ error });
+    };
+};
+exports.sendErrorResponse = sendErrorResponse;
+const getGroupListData = () => {
+    try {
+        const result = [];
+        const innerSelectElement = document.querySelector("#id_listgroups");
+        const optionElementsArr = innerSelectElement.children;
+        for (let i = 0; i < optionElementsArr.length; i++) {
+            const optionElement = optionElementsArr[i];
+            result.push({
+                text: optionElement.textContent || "",
+                value: optionElement.value,
+            });
+        }
+        return result;
+    }
+    catch (e) {
+        return [];
+        throw e;
+    }
+};
+exports.getGroupListData = getGroupListData;
+const getScheduleData = () => {
+    try {
+        const CONTAINER_SELECTOR = ".urk_shedule";
+        const CURRENT_DATE_SELECTOR = ".urk_sheduledate";
+        const CURRENT_LESSON_SELECTOR = ".urk_lessonblock";
+        const CURRENT_LESSON_TIME_SELECTOR = ".urk_timewindow";
+        const CURREN_LESSON_DESCRIPTION_SELECTOR = ".urk_lessondescription";
+        const schedulesData = [];
+        const schedulesElement = document.querySelector(CONTAINER_SELECTOR);
+        if (!schedulesElement) {
+            return schedulesData;
+        }
+        const schedulesChildren = schedulesElement.children;
+        for (let i = 0; i < schedulesChildren.length; i++) {
+            const scheduleItem = { date: "", lessons: [] };
+            const scheduleItemElement = schedulesChildren[i];
+            const date = scheduleItemElement.querySelector(CURRENT_DATE_SELECTOR);
+            scheduleItem.date = date?.textContent || "";
+            const lessonsNodeArr = scheduleItemElement.querySelectorAll(CURRENT_LESSON_SELECTOR);
+            for (let j = 0; j < lessonsNodeArr.length; j++) {
+                const lesson = {
+                    count: null,
+                    timeStart: null,
+                    timeEnd: null,
+                    name: null,
+                };
+                const currentLessonElem = lessonsNodeArr[j];
+                const lessonNameElement = currentLessonElem.querySelector(CURREN_LESSON_DESCRIPTION_SELECTOR);
+                const lessonTimeElement = currentLessonElem.querySelector(CURRENT_LESSON_TIME_SELECTOR);
+                const lessonTimeChildren = lessonTimeElement?.children || [];
+                lesson.name = lessonNameElement?.textContent || "";
+                lesson.count = lessonTimeChildren[0]?.textContent;
+                lesson.timeStart = lessonTimeChildren[1]?.textContent;
+                lesson.timeEnd = lessonTimeChildren[2]?.textContent;
+                scheduleItem.lessons.push(lesson);
+            }
+            schedulesData.push(scheduleItem);
+        }
+        return schedulesData;
+    }
+    catch (e) {
+        return [];
+        throw e;
+    }
+};
+exports.getScheduleData = getScheduleData;
+const setScheduleDB = async (result) => {
+    await app_1.ScheduleModel.destroy({ where: { id: 1 } });
+    const schedule = await app_1.ScheduleModel.build({
+        id: 1,
+        ruUpdateTime: new Date().toString(),
+        result,
+    });
+    await schedule.save();
+};
+exports.setScheduleDB = setScheduleDB;
+//# sourceMappingURL=services.js.map
