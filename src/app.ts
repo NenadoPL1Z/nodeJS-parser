018445c996f6f53c79a72133c2dc9ac1931e7636
NenadoPL1Z <limon4ikpro1@gmail.com>
2023-06-04 16:09:17 +0500

    update: sync

--- src/app.ts
+++ src/app.ts
@@ -63,7 +63,6 @@ app.listen(PORT, async () => {
 
   try {
     await sequelize.authenticate();
-    await sequelize.sync({ force: true });
     console.log("Connection has been established successfully.");
   } catch (error) {
     console.error("Unable to connect to the database:", error);
